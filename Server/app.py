55bf789bf348e352071a6f4803bea6158d1a8e1a
Moin Rauf <147373573+MoinRauf@users.noreply.github.com>
2025-04-27 17:06:22 +0500

    update  cors

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS  # Import CORS
 from venti.venti_api import venti_api  # Import the Venti API Blueprint
 from mri.mri_api import mri_api
 from patient.patient_api import patient_api  # Import the Patient API Blueprint
 
 app = Flask(__name__)
+
+# Enable CORS for all routes
+CORS(app)
 
 # Register the Blueprints for each API
 app.register_blueprint(venti_api, url_prefix='/venti')  # Add a prefix for clarity
